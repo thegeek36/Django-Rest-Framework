88b5c32ecc283e3363c0b07f7f43277060327565
Priyanshu <priyanshupanda36@gmail.com>
2023-09-26 02:50:13 +0530

    Day 6

---
+++ core7/api/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
+from .models import Student
+from .serializer import StudentSerializer
+
+#ViewSet Class Definition
+class StudentViewSet(viewsets.ViewSet):
+    def list(self,request):
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu,many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self,request,pk=None):
+        id = pk
+        if id is not None:
+            stu = Student.objects.get(id = id)
+            serializer = StudentSerializer(stu)
+            return Response(serializer.data)
+    
+    def create(self,request):
+        serializer = StudentSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self,request,pk):
+        id = pk
+        stu = Student.objects.get(id = id)
+        serializer = StudentSerializer(stu,data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':"Data Updated"})
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def partial_update(self,request,pk):
+        id = pk
+        stu = Student.objects.get(id = id)
+        serializer = StudentSerializer(stu,data = request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':"Data Updated"})
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self,request,pk):
+        id = pk
+        stu =  Student.objects.get(id = id)
+        stu.delete()
+        return Response({'msg':"Data Deleted"})
+
