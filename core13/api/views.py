0cd3068c5bbfb1259a7248aee6980d9d51c306b6
Priyanshu <priyanshupanda36@gmail.com>
2023-10-01 12:09:16 +0530

    Day 10 and 11

---
+++ core13/api/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from .models import Student
+from .serializer import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+#ModelViewSet Class Definition
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    """ We can also write about these classes in settings.py files"""
+    #User Authentication Class Definition
+    #authentication_classes = [MyPermission]
+    #User Permission Class
+    #permission_classes = [IsAuthenticated]
+ 
+
+
+
+
+
