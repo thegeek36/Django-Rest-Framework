2831c6f9fb723812b505a15bb3d75d5ca2257680
Priyanshu <priyanshupanda36@gmail.com>
2023-10-04 11:23:27 +0530

    Day 14

--- core16/api/views.py
+++ core16/api/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .models import Student
+from .serializer import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from api.customauth import CustomAuthentication
 
-# Create your views here.
+
+#ModelViewSet Class Definition
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    """ We can also write about these classes in settings.py files"""
+    #User Authentication Class Definition
+    authentication_classes = [CustomAuthentication]
+    #User Permission Class
+    permission_classes = [IsAuthenticated]
+ 
+
+
+
+
+
--- core16/core16/urls.py
+++ core16/core16/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from api import views
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.routers import DefaultRouter
+from api.auth import CustomAuthToken
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView,TokenVerifyView
+router = DefaultRouter()
+router.register('studentapi',views.StudentModelViewSet,basename='student')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include(router.urls)),
+    path('auth/',include('rest_framework.urls',namespace='rest_framework')),
+    path('gettoken/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('refreshtoken/',TokenRefreshView.as_view(),name='token_refresh'),
+    path('verifytoken/',TokenVerifyView.as_view(),name='token_verify'),
 ]
